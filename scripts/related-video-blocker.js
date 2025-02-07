6ddd9d2614ca8009c3cc5f34104c706b7a1d1199
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-02-07 07:48:31 -0700

    Refactor video renderer manager for YouTube watch pages

--- scripts/related-video-blocker.js
+++ scripts/related-video-blocker.js
@@ -62,37 +62,39 @@ class RelatedVideoRendererSetManager extends VideoRendererSetManager {
 }
 
 class RelatedVideoRendererManager extends VideoRendererManager {
+    #videoRenderer = undefined;
+
     constructor(videoRenderer) {
         super();
-        this.videoRenderer = videoRenderer;
+        this.#videoRenderer = videoRenderer;
         if (!this.#canAccessNecessaryAttributesAndElements()) {
             throw new Error('Cannot instantiate class RelatedVideoRendererManager when ytd-compact-video-renderer has not been fully loaded.');
         }
     }
 
     getTitle() {
-        let titleSpan = this.videoRenderer.querySelector('#video-title');
+        let titleSpan = this.#videoRenderer.querySelector('#video-title');
         let title = titleSpan.getAttribute('title');
         return title;
     }
 
     getTitleContainer() {
-        let titleSpan = this.videoRenderer.querySelector('#video-title');
+        let titleSpan = this.#videoRenderer.querySelector('#video-title');
         return titleSpan;
     }
 
     getChannelName() {
-        let channelNameFormattedString = this.videoRenderer.querySelector('.style-scope ytd-channel-name').querySelector('#text');
+        let channelNameFormattedString = this.#videoRenderer.querySelector('.style-scope ytd-channel-name').querySelector('#text');
         let channelName = channelNameFormattedString.getAttribute('title');
         return channelName;
     }
 
     getThumbnail() {
-        return this.videoRenderer.querySelector('.yt-core-image');
+        return this.#videoRenderer.querySelector('.yt-core-image');
     }
 
     #canAccessNecessaryAttributesAndElements() {
-        let channelNameFormattedString = this.videoRenderer.querySelector('.style-scope ytd-channel-name').querySelector('#text');
+        let channelNameFormattedString = this.#videoRenderer.querySelector('.style-scope ytd-channel-name').querySelector('#text');
         if (!channelNameFormattedString) {
             return false;
         }
