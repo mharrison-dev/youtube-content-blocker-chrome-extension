7591c371cd3f789997c24cc58f2eb53bbf649184
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-07 08:28:22 -0700

    Add feature to block and unblock shorts on the YouTube subscription page

--- scripts/pages/subscription-page.js
+++ scripts/pages/subscription-page.js
@@ -1,6 +1,8 @@
 let subscriptionPageVideoSet = new ItemSet(SubscriptionPageVideoItem);
+let subscriptionPageShortSet = new ItemSet(SubscriptionPageShortItem);
 
 let contentBlocker = new ContentBlocker();
 contentBlocker.observe(subscriptionPageVideoSet);
+contentBlocker.observe(subscriptionPageShortSet);
 contentBlocker.setGetItemDivs(() => document.getElementById('contents'));
 contentBlocker.start();
