e2f35cb772414491966db76176cfb4cdcad8e01b
Michael Harrison <197762860+mharrison-dev@users.noreply.github.com>
2025-03-18 11:40:51 -0600

    Refactor logic in Popup class

--- scripts/pages/popup.js
+++ scripts/pages/popup.js
@@ -1,76 +1,79 @@
 class Popup {
+    #titleKeywordEntry;
+    #channelNameKeywordEntry;
+    #saveButton;
+
+    constructor() {
+        this.#titleKeywordEntry = document.getElementById('title-keywords');
+        this.#channelNameKeywordEntry = document.getElementById('channel-name-keywords');
+        this.#saveButton = document.getElementById('save-button');
+    }
+
     start() {
-        this.#loadKeywords();
+        this.#disableSaveButton();
         this.#setupSaveButton();
+        this.#loadKeywordsFromLocalStorage()
+            .then(() => this.#enableSaveButton());
+    }
+
+    #disableSaveButton() {
+        this.#saveButton.disabled = true;
     }
 
     #setupSaveButton() {
-        let saveButton = document.getElementById('save-button');
-        saveButton.addEventListener('click', () => {
-            let keywords = this.#getKeywordsFromEntries();
-            KeywordPersistence
-                .save(keywords.titleKeywords, keywords.channelNameKeywords)
-                .then(() => this.#sendNotificationAboutLocalStorageUpdate());
+        this.#saveButton.addEventListener('click', () => {
+            this.#saveKeywordsToLocalStorage()
+                .then(() => this.#notifyPagesAboutKeywordUpdate());
         });
     }
 
-    #sendNotificationAboutLocalStorageUpdate() {
+    #saveKeywordsToLocalStorage() {
+        let titleKeywords = this.#getKeywordsFromTitleKeywordEntry();
+        let channelNameKeywords = this.#getKeywordsFromChannelNameKeywordEntry();
+        return KeywordPersistence.save(titleKeywords, channelNameKeywords);
+    }
+
+    #notifyPagesAboutKeywordUpdate() {
         chrome.tabs
             .query({ url: ['https://www.youtube.com/watch?v=*', 'https://www.youtube.com/', 'https://www.youtube.com/feed/subscriptions'] })
             .then((tabs) => {
                 if (tabs) {
                     for (let tab of tabs) {
-                        chrome.tabs.sendMessage(tab.id, this.#getKeywordsFromEntries());
+                        let titleKeywords = this.#getKeywordsFromTitleKeywordEntry();
+                        let channelNameKeywords = this.#getKeywordsFromChannelNameKeywordEntry();
+                        chrome.tabs.sendMessage(tab.id, { titleKeywords: titleKeywords, channelNameKeywords: channelNameKeywords });
                     }
                 }
             });
     }
 
-    #getKeywordsFromEntries() {
-        let titleKeywordEntry = document.getElementById('title-keywords');
-        let titleKeywords = this.#getKeywordsFromEntry(titleKeywordEntry);
-
-        let channelNameKeywordEntry = document.getElementById('channel-name-keywords');
-        let channelNameKeywords = this.#getKeywordsFromEntry(channelNameKeywordEntry);
-
-        let keywords = { titleKeywords: titleKeywords, channelNameKeywords: channelNameKeywords };
-        return keywords;
-    }
-
-    #getKeywordsFromEntry(entry) {
-        let emptyStringRegex = /^\s*$/;
-        if (emptyStringRegex.test(entry.value)) {
-            return [];
-        }
-
-        let keywords = entry.value
+    #getKeywordsFromTitleKeywordEntry() {
+        return this.#titleKeywordEntry.value
             .split(',')
-            .map((keyword) => keyword.trim());
-
-        return keywords;
+            .map((keyword) => keyword.trim())
+            .filter((keyword) => keyword.length > 0);
     }
 
-    #loadKeywords() {
-        KeywordPersistence
+    #getKeywordsFromChannelNameKeywordEntry() {
+        return this.#channelNameKeywordEntry.value
+            .split(',')
+            .map((keyword) => keyword.trim())
+            .filter((keyword) => keyword.length > 0);
+    }
+
+    #loadKeywordsFromLocalStorage() {
+        return KeywordPersistence
             .loadKeywords()
             .then((keywords) => this.#addKeywordsToEntries(keywords));
     }
 
     #addKeywordsToEntries(keywords) {
-        let titleKeywordEntry = document.getElementById('title-keywords');
-        this.#addKeywordsToEntry(keywords.titleKeywords, titleKeywordEntry);
-
-        let channelNameKeywordEntry = document.getElementById('channel-name-keywords');
-        this.#addKeywordsToEntry(keywords.channelNameKeywords, channelNameKeywordEntry);
+        this.#titleKeywordEntry.value = keywords.titleKeywords.join(', ');
+        this.#channelNameKeywordEntry.value = keywords.channelNameKeywords.join(', ');
     }
 
-    #addKeywordsToEntry(keywords, entry) {
-        for (let i = 0; i < keywords.length; i++) {
-            entry.value += keywords[i];
-            if (i < keywords.length - 1) {
-                entry.value += ', ';
-            }
-        }
+    #enableSaveButton() {
+        this.#saveButton.disabled = false;
     }
 }
 
